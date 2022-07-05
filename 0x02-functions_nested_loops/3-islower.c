b766ac9def80e16830ba44be3c622483648f18ba
Ayomide Popoola <popoolaayomide234@gmail.com>
2022-07-05 05:45:34 -0700

    new commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - a function that checks for lowercase character
- * c: character to check
+ * @c: character to check
  * Return: 1 if 'c' is lowercase, 0 if otherwise
  */
 int _islower(int c)
