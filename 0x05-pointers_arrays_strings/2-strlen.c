59872098524fd4c07258ad697265b59b3be088db
Ayomide Popoola <popoolaayomide234@gmail.com>
2022-07-12 09:51:38 -0700

    a c program to get the length of a word

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to find length of
+ *
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+int count = 0;
+while (*s != ‘\0’)
+{
+count++;
+p++;
+}
+return (count);
+}
